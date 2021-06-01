4b840bdf56be8fe02407605d4f41bc17cb921779
Dherkyan Ribeiro <dherkyan@gmail.com>
2021-06-01 07:32:59 -0300

    Anotando a classe Filme

--- src/main/java/com/cinema/filmestore/entity/Filme.java
+++ src/main/java/com/cinema/filmestore/entity/Filme.java
@@ -2,14 +2,34 @@ package com.cinema.filmestore.entity;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
+@Table(name = "tb01_filme")
 public class Filme {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "filme_id")
+    private Long filmeId;
+
+    @Column(name = "titulo", length = 100)
     private String titulo;
+
+    @Column(name = "enredo")
     private String enredo;
+
+    @Column(name = "sinopse")
     private String sinopose;
+
+    @Column(name = "ano_lancamento")
     private Integer anoLancamento;
+
+    @Column(name = "imdb")
     private Double imdb;
+
+    @Column(name = "classificacao_indicativa")
     private Integer classificacaoIndicativa;
 
 }
