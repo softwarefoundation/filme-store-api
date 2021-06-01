d08dcf422339091eafebd6d373aa4fd54c072699
Dherkyan Ribeiro <dherkyan@gmail.com>
2021-06-01 07:16:57 -0300

    Inicio projeto Filme Store API

---
+++ src/main/java/com/cinema/filmestore/FilmeStoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.cinema.filmestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmeStoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmeStoreApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cinema/filmestore/FilmeStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinema.filmestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmeStoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
