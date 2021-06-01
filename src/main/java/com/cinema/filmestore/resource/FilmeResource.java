a42d72c0f5692c8e4ac5d21d86e38e4c187d17f8
Dherkyan Ribeiro <dherkyan@gmail.com>
2021-06-01 15:44:56 -0300

    Criando Filme Service

--- src/main/java/com/cinema/filmestore/resource/FilmeResource.java
+++ src/main/java/com/cinema/filmestore/resource/FilmeResource.java
@@ -4,10 +4,9 @@ import com.cinema.filmestore.dto.FilmeDto;
 import com.cinema.filmestore.service.FilmeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/filme")
@@ -21,5 +20,25 @@ public class FilmeResource {
        FilmeDto filme = filmeService.salvarFilme(filmeDto);
        return ResponseEntity.ok(filme);
     }
+
+    @GetMapping(value = "/{filmeId}")
+    public ResponseEntity<?> pesquisarFilmePorId(@PathVariable Long filmeId){
+        FilmeDto filmeDto = filmeService.buscarFilmePorId(filmeId);
+        return ResponseEntity.ok(filmeDto);
+    }
+
+    @GetMapping("/todos")
+    public ResponseEntity<?> listarTodosFilmes(){
+        List<FilmeDto> filmesDto = filmeService.listarTodosFilmes();
+        return ResponseEntity.ok(filmesDto);
+    }
+
+    @DeleteMapping
+    public void deletarFilme(@RequestParam Long filmeId){
+        FilmeDto filmeDto = new FilmeDto();
+        filmeDto.setFilmeId(filmeId);
+        filmeService.deletarFilme(filmeDto);
+    }
+
 
 }
